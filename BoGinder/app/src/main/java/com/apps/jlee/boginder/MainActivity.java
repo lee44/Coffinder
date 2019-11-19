ba558248d22f6347698a0bed8885a27711c25802
Lee44 <jlee7772@yahoo.com>
2019-11-19 14:51:41 -0800

    Lesson 5 done

--- BoGinder/app/src/main/java/com/apps/jlee/boginder/MainActivity.java
+++ BoGinder/app/src/main/java/com/apps/jlee/boginder/MainActivity.java
@@ -1,14 +1,25 @@
 package com.apps.jlee.boginder;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
@@ -17,8 +28,11 @@ public class MainActivity extends AppCompatActivity
 {
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
-    SwipeFlingAdapterView flingContainer;
+    private SwipeFlingAdapterView flingContainer;
+    private FirebaseAuth firebaseAuth;
+    private Button sign_out_button;
     private int i;
+    private String user_gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,18 +40,14 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         al = new ArrayList<>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
-        al.add("html");
-        al.add("c++");
-        al.add("css");
-        al.add("javascript");
+        checkUserGender();
 
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.item, R.id.helloText, al);
         flingContainer = findViewById(R.id.frame);
+        sign_out_button = findViewById(R.id.sign_out_button);
 
         flingContainer.setAdapter(arrayAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener()
@@ -45,10 +55,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void removeFirstObjectInAdapter()
             {
-                // this is the simplest way to delete an object from the Adapter (/AdapterView)
-                Log.d("LIST", "removed object!");
-                al.remove(0);
-                arrayAdapter.notifyDataSetChanged();
+
             }
 
             @Override
@@ -69,11 +76,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter)
             {
-                // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
-                arrayAdapter.notifyDataSetChanged();
-                Log.d("LIST", "notified");
-                i++;
+
             }
 
             @Override
@@ -94,10 +97,115 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        sign_out_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                logoutUser(view);
+            }
+        });
     }
 
     static void makeToast(Context ctx, String s)
     {
         Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void logoutUser(View view)
+    {
+        firebaseAuth.signOut();
+        Intent intent = new Intent(MainActivity.this,LoginorRegisterActivity.class);
+        startActivity(intent);
+        finish();
+        return;
+    }
+
+    public void checkUserGender()
+    {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference maleDB = FirebaseDatabase.getInstance().getReference().child("Users").child("Male");
+        DatabaseReference femaleDB = FirebaseDatabase.getInstance().getReference().child("Users").child("Female");
+
+        maleDB.addChildEventListener(new ChildEventListener()
+        {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
+            {
+                if(dataSnapshot.getKey().equals(firebaseUser.getUid()))
+                {
+                    user_gender = "Male";
+                    getOppositeGender();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot){}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){}
+        });
+
+        femaleDB.addChildEventListener(new ChildEventListener()
+        {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
+            {
+                if(dataSnapshot.getKey().equals(firebaseUser.getUid()))
+                {
+                    user_gender = "Female";
+                    getOppositeGender();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot){}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){}
+        });
+    }
+
+    public void getOppositeGender()
+    {
+        DatabaseReference oppositeGenderDB = FirebaseDatabase.getInstance().getReference().child("Users").child(user_gender == "Male" ? "Female" : "Male");
+
+        oppositeGenderDB.addChildEventListener(new ChildEventListener()
+        {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
+            {
+                if(dataSnapshot.exists())
+                {
+                    al.add(dataSnapshot.child("name").getValue().toString());
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot){}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){}
+        });
     }
 }
