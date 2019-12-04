e93f160e4e69677cd591cef728471c8bb1755882
Lee44 <jlee7772@yahoo.com>
2019-12-03 16:58:22 -0800

    Added a firebase messaging service. Need to store device token into database.

---
+++ BoGinder/app/src/main/java/com/apps/jlee/boginder/Firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,61 @@
+package com.apps.jlee.boginder.Firebase;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+import com.apps.jlee.boginder.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import java.util.Random;
+import androidx.core.app.NotificationCompat;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService
+{
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage)
+    {
+        Log.d("MSG",remoteMessage.getNotification().getBody());
+        shownotification(remoteMessage.getNotification());
+    }
+
+    @Override
+    public void onNewToken(String s)
+    {
+        super.onNewToken(s);
+        Log.d("NEW_TOKEN",s);
+    }
+
+    public void shownotification(RemoteMessage.Notification message)
+    {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.apps.jlee.boginder"; //your app package name
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Techrush Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(message.getTitle())
+                .setContentText(message.getBody())
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+    }
+}
