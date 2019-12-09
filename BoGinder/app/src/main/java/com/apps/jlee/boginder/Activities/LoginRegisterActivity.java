f849af2765c59c1e3a9e9b5e0510c91508fb4954
Lee44 <jlee7772@yahoo.com>
2019-12-09 10:58:45 -0800

    Toolbar working

--- BoGinder/app/src/main/java/com/apps/jlee/boginder/Activities/LoginRegisterActivity.java
+++ BoGinder/app/src/main/java/com/apps/jlee/boginder/Activities/LoginRegisterActivity.java
@@ -27,7 +27,6 @@ public class LoginRegisterActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
-        getSupportActionBar().hide();
 
         ButterKnife.bind(this);
 
